a9143e43f2fe2506e76239f9de4e6e2c419bf97d
AlekseiNikolaevichMedvedev <68754136+AlekseiNikolaevichMedvedev@users.noreply.github.com>
2026-09-16 11:13:19 +0300

    Add files via upload

---
+++ 1/3.py
@@ -0,0 +1,14 @@
+# todo: Данные две переменные:
+# age = 36.6
+# temperature = 25
+# Нужно обменять значения переменных местами. В итого age
+# должен равнятся 25 а temperature – 36.6:
+
+age = 36.6
+temperature = 25
+
+print(f""" age = {age}, temperature = {temperature}""")
+
+age, temperature = temperature, age
+
+print(f""" age = {age}, temperature = {temperature}""")
